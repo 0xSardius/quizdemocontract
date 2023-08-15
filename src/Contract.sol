574e7e144d6f19631f7e4ecd58e84dbcb0264e4a
Sardius <0xsardius@gmail.com>
2023-08-14 20:24:06 -0400

    Updated constructor on the quiz contract

--- src/Contract.sol
+++ src/Contract.sol
@@ -2,5 +2,22 @@
 pragma solidity ^0.8.13;
 
 contract Contract {
-    constructor() {}
+    constructor() {
+        struct Quiz {
+            string question;
+            string[] options;
+            uint256 correctAnswerIndex;
+        }
+
+        Quiz public quiz;
+
+        mapping(address => bool) public hasAnswered;
+        mapping(address => bool) public isCorrect;
+
+        constructor(string memory _question, string[] memory _options, uint256 _correctAnswerIndex) {
+            quiz = Quiz(_question, _options, _correctAnswerIndex);
+        }
+
+    }
 }
+
