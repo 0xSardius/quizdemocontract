17eaad0e1351f9d0aba1b9f1e9107c3dd6a0db5d
Sardius <0xsardius@gmail.com>
2023-08-16 20:41:13 -0400

    gm

--- src/Contract.sol
+++ src/Contract.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 contract Contract {
-    constructor() {
+
         struct Quiz {
             string question;
             string[] options;
@@ -15,9 +15,26 @@ contract Contract {
         mapping(address => bool) public isCorrect;
 
         constructor(string memory _question, string[] memory _options, uint256 _correctAnswerIndex) {
-            quiz = Quiz(_question, _options, _correctAnswerIndex);
+            // require that quiz has at least two options
+            require(_options.length >= 2, "Must have at least 2 options");
+            require(_correctAnswerIndex < _options.length, "Correct answer index must be less than the number of options")
+
+            quiz = Quiz({
+                question: _question,
+                options: _options,
+                correctAnswerIndex: _correctAnswerIndex
+            });
+        }
+
+        function answerQuestion(uint256 _answerIndex) public {
+            require(!hasAnswered[msg.sender], "You have already answered the question");
+            require(_answerIndex < quiz.options.length, "Answer index must be less than the number of options");
+
+            hasAnswered[msg.sender] = true;
+            if[_answerIndex == quiz.correctAnswerIndex] {
+                isCorrect[msg.sender] = true;
+            }
         }
 
     }
-}
 
